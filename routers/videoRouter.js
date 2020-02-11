9eb6505bdc4333b046cebd74e3809385c5061664
tmfljjang <60129793+tmfljjang@users.noreply.github.com>
2020-02-11 15:43:34 +0900

    second.1

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import routes from '../routes';
-import {videos} from '../controllers/videoController';
+import {videos, upload, videoDetail, editVideo, deleteVideo } from '../controllers/videoController';
 const videoRouter = express.Router();
 
 videoRouter.get(routes.videos, videos);
+videoRouter.get(routes.upload, videos);
+videoRouter.get(routes.videoDetail, videos);
+videoRouter.get(routes.editVideo, videos);
+videoRouter.get(routes.deleteVideo, videos);
 
 export default videoRouter;
